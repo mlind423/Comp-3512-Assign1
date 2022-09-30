d725ef8bb914cfc4c37130496fb921af6a92462a
mlind423 <77463748+mlind423@users.noreply.github.com>
2022-09-29 19:04:53 -0600

    Added starting files
    Created all the starting files

---
+++ PHP/DatabaseHelper.php
@@ -0,0 +1,35 @@
+<?php
+class Databasehelper{
+    //Returns a connection object to a database 
+    public static function createConnection($value=array()){
+        $connString = $value[0];
+        $user = $value[1];
+        $password = $value[2];
+        $pdo = new PDO($connString,$user,$password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        return $pdo;
+        }
+    public static function runQuery($connection, $sql, $parameters){
+        $s = null; //$s == Statement
+        //if there are parameters then do a prepared statement
+        if(isset($parameters)){
+            //ensure parameters are in an array
+            if(!is_array($parameters)){
+                $parameters = array($parameters);
+            }
+            // use prepared statement if parameters
+            $s = $connection->prepare($sql);
+            $executedOk = $s->execute($parameters);
+            if(!$executedOk) throw new PDOException;
+        }
+        else{
+            //execute normal query
+            $s = $connection->query($sql);
+            if (!$s) throw new PDOException;
+        }
+        return $s;
+    }
+
+}
+?>
