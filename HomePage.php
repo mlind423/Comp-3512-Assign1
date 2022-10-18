238f844cff1ed46dbb09ebe09d2c200b63e502c0
mlind423 <77463748+mlind423@users.noreply.github.com>
2022-10-18 17:22:07 -0600

    homepage update

--- HomePage.php
+++ HomePage.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,7 +10,28 @@
 <body>
     <?php include('Header.php')?>   
         <div>
-
+        <a href="featured.php?feat=top_genres">Top Genres</a>
+        </div>
+        <div>
+        <a href="featured.php?feat=top_artists">Top Artists</a>
+        </div>
+        <div>
+        <a href="featured.php?feat=most_pop_songs">Most Popular Songs</a>
+        </div>
+        <div>
+        <a href="featured.php?feat=one_hit_wonder">One Hit Wonders</a>
+        </div>
+        <div>
+        <a href="featured.php?feat=longest_acoustic">Longest Acoutic Songs</a>
+        </div>
+        <div>
+        <a href="featured.php?feat=at_the_club">At the Club</a>
+        </div>
+        <div>
+        <a href="featured.php?feat=running_songs">Running Songs</a>
+        </div> 
+        <div>
+        <a href="featured.php?feat=studying">Studying</a>
         </div>
     <?php include('Footer.php')?>
 </body>
