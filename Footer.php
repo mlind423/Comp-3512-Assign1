2d15b34dc4cacd38634f55180a1c5e44fc507263
Spencerreid559 <sreid559@mtroyal.ca>
2022-10-21 15:29:33 -0600

    Fixed some minor CSS

--- Footer.php
+++ Footer.php
@@ -3,9 +3,11 @@
     <footer>
         <h4>COMP 3512</h4>
         <p id="copyright">©Matthew, Spencer</p>
-        <a href="https://github.com/mlind423/Comp-3512-Assign1">Repository</a>
-        <a href="https://github.com/Spencerreids">Spencer's github</a>
-        <a href="https://github.com/mlind423">Matthew's github</a>
+        <div class="Github">
+            <a href="https://github.com/mlind423/Comp-3512-Assign1">Repository</a>
+            <a href="https://github.com/Spencerreids">Spencer's github</a>
+            <a href="https://github.com/mlind423">Matthew's github</a>
+        </div>
     </footer>
 
 
